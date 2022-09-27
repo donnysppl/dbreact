8269009533813061bc9ce307ed3b2d38ee6d9f0c
donnysppl <donny@sppl.ind.in>
2022-09-27 15:46:59 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Services from "./Pages/Services";
 import Warranty from "./Pages/Warranty";
 import Error from "./Pages/Error";
 import Product from './Pages/Product';
-import Product_detail from './Pages/Product_detail';
+// import Product_detail from './Pages/Product_detail';
 
 
 
@@ -17,14 +17,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={ <Home /> } />
-            <Route path="about" element={ <About/> } />
-            <Route path="support" element={ <Support/> } />
-            <Route path="services" element={ <Services/> } />
-            <Route path="warranty-information" element={ <Warranty/> } />
-            <Route path="product" element={ <Product/> } />
-            <Route path="product-detail" element={ <Product_detail/> } />
+            <Route path="/about" element={ <About/> } />
+            <Route path="/support" element={ <Support/> } />
+            <Route path="/services" element={ <Services/> } />
+            <Route path="/warranty-information" element={ <Warranty/> } />
+            <Route path="/product" element={ <Product/> } />
+            {/* <Route path="product-detail" element={ <Product_detail/> } /> */}
             <Route path="*" element={ <Error/> } />
-          {/* </Route> */}
+     
         </Routes>
       </BrowserRouter>
     </>
